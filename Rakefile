afb37b4cc569e658501a6a96bcbb13dd807ef675
Marcelo Junior <marcelo.jr63@gmail.com>
2023-04-04 21:55:02 -0300

    Fix bug when file does not exist and add tests

--- Rakefile
+++ Rakefile
@@ -1,6 +1,13 @@
+require "rake/testtask"
 require "rake/extensiontask"
 
 Rake::ExtensionTask.new "musicrb" do |ext|
   ext.lib_dir = "lib/musicrb"
 end
 
+Rake::TestTask.new do |t|
+  t.libs << "test"
+end
+
+desc "Run tests"
+task default: :test
--- ext/musicrb/musicrb.c
+++ ext/musicrb/musicrb.c
@@ -1,53 +1,90 @@
 #include <ruby/ruby.h>
+#include <unistd.h>
 #include <vlc/vlc.h>
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 VALUE rb_cMusic;
 
 libvlc_instance_t* inst;
+libvlc_media_t* m;
 libvlc_media_player_t* mp;
+
+VALUE
+rb_music_meta(VALUE msc)
+{
+  if(m == NULL || mp == NULL) return Qnil;
+
+  libvlc_state_t state = libvlc_media_player_get_state(mp);
+  if(state != libvlc_Playing) return Qnil;
+
+  char *title = libvlc_media_get_meta(m, libvlc_meta_Title);
+  char *artist = libvlc_media_get_meta(m, libvlc_meta_Artist);
+  char *album = libvlc_media_get_meta(m, libvlc_meta_Album);
+  char *genre = libvlc_media_get_meta(m, libvlc_meta_Genre);
+
+  if(title == NULL || artist == NULL || album == NULL || genre == NULL) return Qnil;
+
+  VALUE meta = rb_hash_new();
+  rb_hash_aset(meta, ID2SYM(rb_intern("title")), rb_str_new_cstr(title));
+  rb_hash_aset(meta, ID2SYM(rb_intern("artist")), rb_str_new_cstr(artist));
+  rb_hash_aset(meta, ID2SYM(rb_intern("album")), rb_str_new_cstr(album));
+  rb_hash_aset(meta, ID2SYM(rb_intern("genre")), rb_str_new_cstr(genre));
+
+  return meta;
+}
 
 VALUE
 rb_music_stop(VALUE msc)
 {
+  if(m == NULL || mp == NULL) return Qnil;
+  
   /* Stop playing */
   libvlc_media_player_stop(mp);
- 
+
+  /* Free the media */
+  libvlc_media_release(m);
+
   /* Free the media_player */
   libvlc_media_player_release(mp);
+  
+  m = NULL;
   mp = NULL;
 
-  return msc;
+  return Qnil;
 }
 
 VALUE
 rb_music_play(VALUE msc, VALUE rpath)
 {
-  libvlc_media_t* m;
+  /* Stop music if already playing */
+  if(mp != NULL) {
+    rb_music_stop(msc);
+  }
 
   /* Convert Ruby String to C String */
   char *cpath = rb_string_value_cstr(&rpath);
 
+  /* Check if rpath is a valid path of file */
+  if(access(cpath, F_OK) != 0) {
+    rb_raise(rb_eArgError, "File does not exist");
+  }
+
   /* Create a new media */
   m = libvlc_media_new_path(inst, cpath);
-
-  /* Stop music if already playing */
-  if(mp != NULL){
-    rb_music_stop(msc);
-  }
 
   /* Create a media player */
   mp = libvlc_media_player_new_from_media(m);
 
-  /* No need to keep the media now */
-  libvlc_media_release(m);
- 
-  /* play the media_player */
+  /* Play the media_player */
   libvlc_media_player_play(mp);
  
-  return msc;
+  /* Wait the media_player */
+  while(libvlc_media_player_get_state(mp) != libvlc_Playing);
+
+  return rb_music_meta(msc);
 }
 
 void
@@ -59,4 +96,5 @@ Init_musicrb(void)
   rb_cMusic = rb_define_class("Music", rb_cObject);
   rb_define_singleton_method(rb_cMusic, "play", rb_music_play, 1);
   rb_define_singleton_method(rb_cMusic, "stop", rb_music_stop, 0);
+  rb_define_singleton_method(rb_cMusic, "meta", rb_music_meta, 0);
 }
